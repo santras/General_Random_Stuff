b8c8357d57146b6ec501bcb4ef7736ec987f36db
Sanna <31210400+santras@users.noreply.github.com>
2017-12-13 17:02:00 +0200

    Add files via upload

--- new_cmems_tohourly.py
+++ new_cmems_tohourly.py
@@ -15,21 +15,22 @@ from scipy import stats
 
 
 headerfilename=('header.txt')                       # Header titles as a txt file, should be in the working directory
-path="..\Data\MEMS_GL\\"     # Path for the original data file folder, best not to have anything else
-output_path= "..\..\Data_new\CMEMS_hourly_fast_check\\"      # than the .txt data file in this directory nop= no parsing empties
+path="..\Data\MEMS_GL\\"                            # Path for the original data file folder, best not to have anything else
+output_path= "..\..\Data_new\CMEMS_hourly\CMEMS_hourly_fast_check_nop\\"        # than the .txt data file in this directory
 time_difference=60                                  # Time difference in minutes between measurements
-max_sealevel_difference=50                          # In cm
-check_files=True                          # Normally True, only false if you don't want the check to be made
-patching_missing=True                     # Normally True, only false if you don't want the check to be made
+max_sealevel_difference=50                          # Sea level change in 1 hour that is considered too suspicious, in cm
+check_files=True                          # Normally True, only false if you don't want the check to be made (check= remove suspicious)
+patching_missing=False                     # Normally True, only false if you don't want to add missing measurements as nan-values
 
-# Function 2
+
+
+# Function 1
 def open_rfile(file_name):
     # Called by: get_headers / Function 2,Calls: -
     #Opens a readable file and reads it
     try:
         file=open(file_name,'r')
         data=file.readlines()
-        #print(len(data))
         file.close()
         ok=True
     except:
@@ -54,7 +55,7 @@ def get_headers():
     order=[]
     for line in headerfile:                 # Reads the lines of the file into a dictionary
         if not (line.strip() == "" or line.strip()[0] == "#"):
-            if ":" in line:                     # Some unchanging variables are saved in into the dictionary from the headerfile
+            if ":" in line:
                 splitline = line.split(":")
                 key = splitline[0].strip()
                 value = splitline[1].strip()
@@ -67,13 +68,11 @@ def get_headers():
 
 # Function 3
 def open_cmemsfiles(file,header):
-
     # Called by: Main , Calls: update_header / Function 4
     # Opens CMEMS  files, then reads the data and maybe updates the header, Checks the sea level data
     (data,okey) = open_rfile(file)
 
     # taking values and order from header
-
     for line in (data[0:20]):
         if not (line.strip() == "" or line.strip()[0] == "#"):
             splitline=line.split()
@@ -93,13 +92,14 @@ def open_cmemsfiles(file,header):
                     source=""
                 else:
                     source=splitline[1].strip()
+    # Update the heder
+    (header)=update_header(header, station, lat, lon, datum, source) # Function 4
 
-    (header)=update_header(header, station, lat, lon, datum, source)
-    #print(header)
-
-    if not (data[20][0]=="-"):    # juts an extra check that header size is ok
+    if not (data[20][0]=="-"):    # juts an extra check that header size is normal
         print("Header size of file is not expected in file: ",file)
         exit()
+
+    # Reading the file
     date=[]
     time=[]
     slev=[]
@@ -117,6 +117,7 @@ def open_cmemsfiles(file,header):
         splittime=((time[ind]).split(":"))
         thisdate=(datetime.datetime(int(splitdate[0]),int(splitdate[1]),int(splitdate[2]),int(splittime[0]),
                                      int(splittime[1])))
+        # Making sure that if value=nan then flag=9 and vise versa
         if np.isnan(slev[ind]) or qual[ind]==9:
             variables.append([thisdate, np.nan, 9])
         else:
@@ -130,7 +131,6 @@ def update_header(Headers, stationname, latitude, longitude,datum,source):
     #Called by: open_ncfiles/Function3,  Calls:-
     # Updates header info
     # like in tgtools
-    # later, start, end, total, missing
     Headers["Source"] = source
     Headers["Datum"] = datum
     Headers["Station"] = stationname
@@ -150,51 +150,43 @@ def process_file(filename,sl_variables,Headers,order,station):
     # Makes an output folder if it doesen't exist and then calls in functions to check that data is in order
     # and creates needed variables for header, then it updates and orders the header and finally writes the output.
 
-
     output_file=output_path+"Gl_"+station.replace(" ", "")+".txt"                   # HERE OUTPUTFILENAME
-    # output_file=output_path+"Gl_"+filename[:-3]+".txt"                             # replace takes away empty spaces
 
-    #print("outputpath",output_path)
-    if not os.path.exists(output_path):                             # Making the output folder if needed
+    if not os.path.exists(output_path):                             # Making the output folder if it doesen't allready exist
         os.makedirs(output_path, exist_ok=True)
 
-
-
+    # Not writing the outputfile if no data
     if len(sl_variables)==0:
         print("File "+filename+" was empty." )
         return
 
-    (sl_variables, missing, tot_values, start, end)=check_data(filename,sl_variables)   # Function 6 ##########Here
-    #print(tot_values)
+    (sl_variables, missing, tot_values, start, end)=check_data(filename,sl_variables)   # Function 6
 
+    # If after check (and taking the hourly measurements) no data left then not writing the output
     if tot_values==0:
         print("Coudn't find at the hour measurements in file " + filename + " .")
         return
-    #print(Headers)
-    #print(output_file)
-    Headers_filled=fill_headers(Headers,missing,tot_values,start,end)               # Function 9
-    #print(Headers_filled)
-    write_output(Headers_filled,sl_variables,order,output_file)                             # Function 10
-    #print(output_file)
+
+    Headers_filled=fill_headers(Headers,missing,tot_values,start,end)               # Function 8
+    write_output(Headers_filled,sl_variables,order,output_file)                     # Function 9
 
 #Function 6
 def check_data(name,sl_variables):
     # Called by: process file/Function 5, Calls: check_listorder / function 7
-    # Checks the data, if multible measurements from same time_difference keeps only the "at the hour" measurement.  Checks
-    # if that measurement is okey in value (difference before and after), if not (or missing), checks the mean of closest
-    # values, if that okey, replaces the measurement with mean. Checks order, inserts missing values, counts number of
-    # total values, missing, start date and end date
-                    # Transferring only the at the hour measurements to var_onlyhour
+    # Checks the data, keeps only at the hour measurements, checks the data, fills in the missing (with nans).
+    # Checks order, inserts missing values, counts number of total values, missing, start date and end date.
+
+    # Transferring only the at the hour measurements to var_onlyhour
     missing=0
     var_onlyhour=[]
-    for ind in range(1, len(sl_variables)):
+    for ind in range(len(sl_variables)):
         this_minute = sl_variables[ind][0].strftime("%M")
-        if this_minute == "00":  # When at the hour example 12:000
+        if this_minute == "00":  # When at the hour example 12:00
             if (sl_variables[ind][2]==9):
                 missing=missing+1
             var_onlyhour.append(sl_variables[ind])
 
-# File is empty
+    # If file is empty
     if len(var_onlyhour)==0:
         return [], 0, 0, [], []
 
@@ -205,15 +197,17 @@ def check_data(name,sl_variables):
         return var_onlyhour, missing, len(var_onlyhour), var_onlyhour[0][0], var_onlyhour[-1][0]
 
 
-            # Second check difference between measurements can't be too much
+    # Second check difference between measurements can't be too much
     too_big = []
     for ind in range(1, len(var_onlyhour)):
+        # Checks difference with earlier measurement, if too big -> removes (missing value) measurement
         if (not np.isnan(var_onlyhour[ind][1])) and (not np.isnan(var_onlyhour[ind-1][1])) :
             if np.abs(var_onlyhour[ind][1] - var_onlyhour[ind-1][1]) > (max_sealevel_difference):
                 too_big.append(["B",var_onlyhour[ind-1], var_onlyhour [ind]])
                 var_onlyhour[ind][1]=np.nan
                 var_onlyhour[ind][2]=9
                 missing=missing+1
+        # If earlier was missing, checks the differenc to next one -> removes if not ok
         elif (not np.isnan(var_onlyhour[ind][1])) and (not np.isnan(var_onlyhour[ind+1][1])):
             if np.abs(var_onlyhour[ind][1] - var_onlyhour[ind+1][1]) > (max_sealevel_difference):
                 too_big.append(["A", var_onlyhour[ind+1], var_onlyhour[ind]])
@@ -221,8 +215,9 @@ def check_data(name,sl_variables):
                 var_onlyhour[ind][2]=9
                 missing=missing+1
 
-    print("Too big jumps: ",len(too_big))
+    print("Too big jumps between measurements: ",len(too_big))
 
+    # Ends if don't want to add "missing lines"
     if patching_missing==False:
         return var_onlyhour, missing, len(var_onlyhour), var_onlyhour[0][0], var_onlyhour[-1][0]
 
@@ -240,7 +235,7 @@ def check_data(name,sl_variables):
         # Missing times are patched with nan and flag=9
         # Passing the first item directly
         new_variables.append(var_onlyhour[0])
-        #   Next ones are checked weather they have the proper time difference or not
+        #   Next ones are checked weather they have the proper time difference or not, missing values added
         for ind in range(1, len(var_onlyhour)):
             if var_onlyhour[ind][0] - new_variables[-1][0] > datetime.timedelta(seconds=time_difference * 60):
                 while var_onlyhour[ind][0] - new_variables[-1][0] > datetime.timedelta(seconds=time_difference * 60):
@@ -248,7 +243,7 @@ def check_data(name,sl_variables):
                     missing=missing+1
             new_variables.append(var_onlyhour[ind])
 
-        transposed = []  # Trick to only send timestamps to the check_listorder function 7
+        transposed = []
         for i in range(3):
             transposed.append([row[i] for row in new_variables])
         (inorder, inds) = check_listorder(transposed[0],time_difference * 60)  # Function 7, checking if entries with 60 min time interval
@@ -256,105 +251,6 @@ def check_data(name,sl_variables):
             print("Warning, something went wrong in ordering the file")
 
         return new_variables, missing, len(new_variables), new_variables[0][0], new_variables[-1][0]
-
-    ################ UNCHECKED FROM HERE
-
-    #
-    #
-    #     # If only interested to look at the too big jump points, then run write_files=False
-    #     if write_files == False:
-    #         print("Too big jump in seal level between measurements: ", len(too_big))
-    #         #print("here")
-    #         match_ind=[]
-    #         print("date, sea level, sea level previous hour, sea level next hour")
-    #         if len(too_big) != 0:
-    #             for ind in range(len(too_big)):
-    #                 print(too_big[ind])
-    #                 for index in range(len(sl_variables)):
-    #                     if too_big[ind][0] == sl_variables[index][0]:
-    #                         match_ind = index
-    #                     elif too_big[ind - 1][0] == sl_variables[index][0]:
-    #                         match_ind = index
-    #                         if match_ind < 10:
-    #                             print(sl_variables[0:match_ind + 10])
-    #                         elif match_ind > (len(sl_variables) - 10):
-    #                             print(sl_variables[(match_ind - 10):-1])
-    #                         else:
-    #                             print(sl_variables[match_ind - 10:match_ind + 10])
-    #
-    #         return [], missing, 0, [], []
-    #
-    #
-    # # Checking the order of the new set of measurements, gives false if time interval something else than
-    # # given time interval between measurements
-    # transposed = []  # Trick to only send timestamps to the check_listorder function 7
-    # for i in range(3):
-    #     transposed.append([row[i] for row in var_onlyhour])
-    # (inorder, inds) = check_listorder(transposed[0],
-    #                                   time_difference * 60)  # Function 7, checking if entries with 60 min time interval
-    # if not inorder:
-    #     print(
-    #         "File " + name + " is not in order with the given time interval between measurements, probably missing entries."
-    #                          " Trying to solve the problem.")
-    #
-    #     # Missing times are patched with nan and flag=9
-    #     # Passing the first item directly
-    #     new_variables.append(var_onlyhour[0])
-    #     #   Next ones are checked weather they have the proper time difference or not
-    #     for ind in range(1, len(var_onlyhour)):
-    #         if var_onlyhour[ind][0] - new_variables[-1][0] > datetime.timedelta(seconds=time_difference * 60):
-    #             while var_onlyhour[ind][0] - new_variables[-1][0] > datetime.timedelta(seconds=time_difference * 60):
-    #                 new_variables.append([new_variables[-1][0] + datetime.timedelta(seconds=time_difference * 60), np.nan, 9])
-    #         new_variables.append(var_onlyhour[ind])
-    #
-    #     transposed = []  # Trick to only send timestamps to the check_listorder function 7
-    #     for i in range(3):
-    #         transposed.append([row[i] for row in new_variables])
-    #     (inorder, inds) = check_listorder(transposed[0],
-    #                                   time_difference * 60)  # Function 7, checking if entries with 60 min time interval
-    #     if not inorder:
-    #         print("Warning, something went wrong in ordering the file")
-    #
-    # # Fixes back the values that can be "corrected" with the mean of the hour, but can only do this if new data
-    # # are with smaller freaquency than the originals
-    # if attemp_fix==True:
-    #     if len(new_variables) < len(sl_variables):
-    #         for ind in range(len(new_variables)):
-    #             if new_variables[ind][2] == 9:  # We made sure that all missing are both nan and flag=9
-    #                 helper = []
-    #                 for index in range(len(
-    #                         sl_variables)):  # Going through whole data to find measurements from within the time range
-    #                     if np.abs(sl_variables[index][0] - new_variables[ind][0]) <= datetime.timedelta(
-    #                             seconds=0.5 * time_difference * 60): # If time difference withinh half of the time step of original
-    #                         # print(np.abs(sl_variables[index][0]-new_variables[ind][0]))
-    #                         if sl_variables[index][2] != 9:             # Go through all measurements within the timestep and keep the ones that are okey
-    #                             helper.append(sl_variables[index][1])    # The measurements that are now put in are not necessarily okey.. not checked
-    #                         else:
-    #                             helper.append(np.nan)
-    #                 if (len(helper) == 0):
-    #                     nothing = 1
-    #                 elif (np.isnan(helper).sum() <= 0.5 * len(helper)) and (
-    #                         len(helper) - np.isnan(helper).sum() != 0):  # If less than half values bad to use in mean
-    #                     new_variables[ind][1] = np.nanmean(helper)
-    #                     new_variables[ind][2] = 7
-    #                 elif (len(helper) - np.isnan(helper).sum() != 0):  # Not empty nor only nans
-    #                     new_variables[ind][1] = np.nanmean(helper)
-    #                     new_variables[ind][2] = 8
-    #                 if (not np.isnan(new_variables[ind][1])) and (not np.isnan(new_variables[ind - 1][
-    #                                                                                1])):  # Then lastly testing that the mean replacing the number is reasonable.
-    #                     if np.abs(new_variables[ind][1] - new_variables[ind - 1][1]) > max_sealevel_difference + 20:
-    #                         new_variables[ind][1] = np.nan
-    #                         new_variables[ind][2] = 9
-    #
-    # missing_round2 = 0
-    # for ind in range(len(new_variables)):
-    #     if new_variables[ind][2] == 9:
-    #         missing_round2 = missing_round2 + 1
-    #
-    # first_day = str(new_variables[0][0])
-    # last_day = str(new_variables[-1][0])
-    # return new_variables, missing_round2, len(new_variables), first_day, last_day
-
 
 
 # Function 7
@@ -375,22 +271,17 @@ def check_listorder(dates,diff,current_ind=1):
     return ok, inds
 
 
-
-
-
-# Funtion 9
+# Funtion 8
 def fill_headers(Headers,missing,total,starttime,endtime):
     # Called by: process_file /Function 4 , Calls: -
     # Fills headers
-    # Almost completely like in tgtools
     Headers["Start time"] = starttime
     Headers["End time"] = endtime
     Headers["Total observations"] = str(total)
     Headers["Missing values"] = str(missing)
-
     return Headers
 
-# Function 10
+# Function 9
 def write_output(HeaderDict, sl_variables, order, outputfile):
     # Called by: process_file /Function 4  , Calls: -
     # Writes the output
@@ -399,18 +290,16 @@ def write_output(HeaderDict, sl_variables, order, outputfile):
         # Check for empty header, warn if so.
         print ("! Warning: empty header")
     Header = []
-    #print(order)
-    #print(HeaderDict)
+
     for key in order:
         try:
             Header.append([key, HeaderDict[key]])
         except KeyError:
             Header.append([key, ""])
-    #print(Header)
+
     # The header writing code, contains various checks.
     output=[]
     for line in Header:
-        #print(line)
         # Loop through all header lines
         if not len(line) ==  2:
             # Header line should only have to elements (key and value)
@@ -439,7 +328,6 @@ def write_output(HeaderDict, sl_variables, order, outputfile):
     except:
         print("Couldn't open file to print into.")
 
-    #print(output)
 
     # Writing headers
     file.writelines(output)
@@ -461,7 +349,7 @@ def write_output(HeaderDict, sl_variables, order, outputfile):
     prints=[]
     for ind in range(len(date)):
         prints.append("{}\t{}\t{:6.4}\t{:3}\n".format(date[ind],time[ind],slev[ind],qual[ind]))
-    #print(prints[0:10])
+
     try:
         for ind in range(len(sl_variables)):
             file.write(prints[ind])
